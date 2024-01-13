741c4a114bd777bbdcc47f85228e07169bd64d4c
Mykel-Eze <michaeleze1997@gmail.com>
2024-01-14 00:00:23 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app-wrapper">
+    <RouterView />
+  </div>
+</template>
+
+<style src=".\assets\css\fonts.css"></style>
+<style src=".\assets\css\styles.css"></style>
+
+<style>
+div#nav {
+  float: left;
+  width: 100%;
+  overflow-x: hidden;
+  position: relative;
+  min-height: 100vh;
+}
+
+div#main-contents-wrapper {
+  padding-top: 90px;
+  /* min-height: 100vh; */
+}
+
+@media(max-width: 600px) {
+  div#main-contents-wrapper {
+    padding-top: 70px;
+  }
+}
+</style>
